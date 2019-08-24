c3ad26baf987965b9d0cd689412ecbe76f019fef
Dea7h <emil-n1k@hotmail.com>
2019-08-24 23:00:40 +0200

    Yet another commit

--- App/index.php
+++ App/index.php
@@ -1,35 +1,51 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title><?= WEBSITE_TITLE ?></title>
-        <link rel="stylesheet" href="./public/css/main.css">
-    </head>
-    <body>
-        <section>
-            <header>
-                <div class="websiteName"><?= WEBSITE_NAME ?></div>
-            </header>
-            <nav class="navBar">
-                <div class="bar-title">
-                    Menu
-                </div>
-                <ul>
-                    <li data-link="register">Register</li>
-                    <li data-link="about-us">About Us</li>
-                </ul>
-            </nav>
-            <main>
-                main
-            </main>
-            <aside class="userArea">
-                user
-            </aside>
-            <footer>
-                <div class="title"><?= WEBSITE_FOOTER ?></div>
-                <div class="loadTime">
-                    <output>the page loaded for %s</output>
-                </div>
-            </footer>
-        </section>
+
+<head>
+    <title><?= WEBSITE_TITLE ?></title>
+    <link rel="stylesheet" href="./public/css/main.css">
+</head>
+
+<body>
+    <section>
+        <header>
+            <div class="websiteName"><?= WEBSITE_NAME ?></div>
+        </header>
+        <nav class="navBar">
+            <div class="block-title">Menu</div>
+            <ul>
+                <li data-link="register">Register</li>
+                <li data-link="about-us">About Us</li>
+            </ul>
+        </nav>
+        <main>
+            <div class="block-title">Main content</div>
+            <div class="block-content padding" id="root">
+                ...
+            </div>
+        </main>
+        <aside class="userArea">
+            <div class="block-title">User Area</div>
+            <div class="block-content padding">
+                login form etc...
+            </div>
+            <div class="block-title">Other stuff</div>
+            <div class="block-content padding">
+                something else<br />
+                something else<br />
+                something else<br />
+                something else<br />
+                something else
+            </div>
+        </aside>
+        <footer>
+            <div class="title"><?= WEBSITE_FOOTER ?></div>
+            <div class="loadTime">
+                <output>the page loaded for %s</output>
+            </div>
+        </footer>
+    </section>
     <script type="text/javascript" src="./public/js/main.js"></script>
+</body>
+
 </html>
--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,40 @@
-var links = document.querySelectorAll('[data-link]');
+//Variables
+const mainRoot = document.querySelector('#root')
+const links = document.querySelectorAll('[data-link]')
+const blockTitles = document.querySelectorAll('.block-title')
 
-links.forEach(element => {
-    element.addEventListener('click', function() {
-        console.log(this.dataset.link);
-        var link = this.dataset.link;
-        document.getElementById('content-loader').innerHTML = 'The link [' + link + '] has been clicked';
-    });
-});
+//Functions
+const App = {
+    animate: {
+        show: element => {
+            element.style.display = ''
+        },
+        hide: element => {
+            element.style.cssText = 'display: none;'
+        },
+        toggle: element => {
+            element.style.display !== 'none' ?
+                App.animate.hide(element) :
+                App.animate.show(element)
+        }
+    }
+}
+
+/**
+ * Loop through all links and add a click event listener to them
+ */
+for (let link of links) {
+    link.addEventListener('click', () => {
+        mainRoot.innerHTML = `The link [${link.dataset.link}] has been clicked`
+    })
+}
+
+/**
+ * Making the title bars clickable
+ * and once clicked they hide the content beneath them
+ */
+for (let title of blockTitles) {
+    title.addEventListener('click', () => {
+        App.animate.toggle(title.nextElementSibling)
+    })
+}
