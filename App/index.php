82516f3749effb715d84202b69305079a5695ef6
Emil Nikolov <emiL@emiL-Air711.local>
2019-08-24 18:09:51 +0200

    Initial commit

--- App/index.php
+++ App/index.php
@@ -1,13 +1,43 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" type="text/css" href="./public/css/main.css" />
-</head>
-<body>
-    Welcome home
-</body>
+<html>
+    <head>
+        <title>Welcome to the JavaScript world</title>
+        <link rel="stylesheet" href="./public/css/main.css">
+    </head>
+    <body>
+        <div class="content">
+            <div class="top">
+                <div class="name">
+                    Apple Software Corporation
+                </div>
+            </div>
+
+            <div class="left">
+                <div class="padding">
+                    <div class="menu">
+                        <div data-link="home">Home Page</div>
+                        <div data-link="second">Second Page</div>
+                        <div data-link="third">Third Page</div>
+                        <div data-link="fourth">Fourth Page</div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="right">
+                <div class="content-title">
+                    Stuff shall load here beneath this
+                </div>
+                <div class="padding" id="content-loader">
+                    This is where stuff will be loaded
+                </div>
+            </div>
+            
+            <div class="bottom">
+                <div class="padding">
+                    Created by Dea7h
+                </div>
+            </div>
+        </div>
+    </body>
+    <script type="text/javascript" src="./public/js/javascript.js"></script>
 </html>
